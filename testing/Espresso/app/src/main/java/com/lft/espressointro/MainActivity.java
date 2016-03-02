e5cd987ff9b2cd4d45777fea6cc979af27541a74
laaptu <laaptu@laaptus-MacBook-Pro.local>
2016-03-02 18:08:14 +0545

    Some minor changes

--- testing/Espresso/app/src/main/java/com/lft/espressointro/MainActivity.java
+++ testing/Espresso/app/src/main/java/com/lft/espressointro/MainActivity.java
@@ -1,9 +1,12 @@
 package com.lft.espressointro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import com.lft.espressointro.tasko.TaskMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        startActivity(new Intent(this, TaskMainActivity.class));
-//        this.finish();
+        goToTaskActivity();
+    }
+
+    private void goToTaskActivity(){
+        startActivity(new Intent(this, TaskMainActivity.class));
+        this.finish();
     }
 
     public void btnClick(View view) {
--- testing/Espresso/app/src/main/java/com/lft/espressointro/tasko/TaskMainActivity.java
+++ testing/Espresso/app/src/main/java/com/lft/espressointro/tasko/TaskMainActivity.java
@@ -1,15 +1,100 @@
 package com.lft.espressointro.tasko;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.lft.espressointro.R;
+import com.lft.espressointro.tasko.fragments.MainFragment;
+import com.lft.espressointro.tasko.fragments.NewTaskFragment;
 
 /**
  * Created by laaptu on 3/2/16.
  */
 public class TaskMainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String id = getIntent().getStringExtra("person_id");
+        setContentView(R.layout.activity_taskmain);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setRippleColor(getResources().getColor(R.color.ripple_color));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToNewTask();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+        Log.d("Test", "test");
+
+        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                int backStackCount = getSupportFragmentManager().getBackStackEntryCount();
+                if(backStackCount == 0) {
+                    fab.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        navigateToMainFragment();
+
+    }
+
+    private void navigateToMainFragment() {
+        MainFragment f = MainFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_content, f, MainFragment.class.getSimpleName())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void navigateToNewTask(MenuItem item) {
+        navigateToNewTask();
+    }
+
+    private void navigateToNewTask() {
+        fab.setVisibility(View.GONE);
+        NewTaskFragment f = NewTaskFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_content, f, NewTaskFragment.class.getSimpleName())
+                .addToBackStack(NewTaskFragment.class.getSimpleName())
+                .commit();
     }
 }
